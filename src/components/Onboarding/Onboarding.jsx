a52cbcb0f90f1178eeddcbf1cd2dcbf020903169
Fernando de la Oliva <fernandoliva9@gmail.com>
2021-11-14 15:56:35 +0100

    fix version of swiper .js and testing

--- src/components/Onboarding/Onboarding.jsx
+++ src/components/Onboarding/Onboarding.jsx
@@ -1,36 +1,50 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
-//Import Swiper JS
-import { Swiper, SwiperSlide } from './../../../node_modules/swiper/react/react';
+//CSS
+import 'swiper/swiper.scss';
 
-//Import Swiper SCSS
-import '~/../swiper/swiper.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+import 'swiper/components/a11y/a11y.scss';
 
 //Images
-import onboarding1 from './../../assets/img/undrawGoodDoggy4Wfq.png';
+import onboarding1 from './../../assets/img/onboarding1.png';
+import onboarding2 from './../../assets/img/onboarding2.png';
+import onboarding3 from './../../assets/img/onboarding3.png';
+
+
+
 
 const Onboarding = () => {
-    const swiper = new Swiper('.swiper', {
-        direction: 'vertical',
-        loop: false
-    });
 
     return (
-        <Swiper navigation={true} className="swiper">
-            <SwiperSlide>
-                <img src={onboarding1} alt="onboarding1" />
-                <h5>Encuentra todo tipo de servicios que tienes cerca de ti</h5>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="" alt="onboarding2" />
-                <h5>Adopta desde tu móvil</h5>
-                <p>Puedes acceder al perfil de muchos animales que están en adopción y filtrarlos para encontrar el que mejor se adapte a ti</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src="" alt="onboarding3" />
-                <h5>Si eres una asociación sube a tus peludos para darles más difusión</h5>
-            </SwiperSlide>
-        </Swiper>
+        <div className="onboarding">
+            <Swiper
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                slidesPerView={1}
+                pagination={{"dynamicBullets": true}}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>
+                    <img src={onboarding1} alt="onboarding1" />
+                    <h5>Encuentra todo tipo de servicios que tienes cerca de ti</h5>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={onboarding2} alt="onboarding2" />
+                    <h5>Adopta desde tu móvil</h5>
+                    <p>Puedes acceder al perfil de muchos animales que están en adopción y filtrarlos para encontrar el que mejor se adapte a ti</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={onboarding3} alt="onboarding3" />
+                    <h5>Si eres una asociación sube a tus peludos para darles más difusión</h5>
+                </SwiperSlide>
+            </Swiper>
+            
+        </div>
     )
 }
 
