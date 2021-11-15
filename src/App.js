68a0c218cb3a3f30623317a1354bbe7a58b9e9d1
DavidDlp <david.delpozo@bootcamp-upgrade.com>
2021-11-15 15:49:39 +0100

    FEAT: Before merge David

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './styles/app.scss';
+import React from 'react';
+import Pets from './components/Pets/Pets';
 
 function App() {
   return (
     <div className="App">
-
+      <Pets/>
     </div>
   );
 }
---
+++ src/components/Pets/Pets.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import 'swiper/swiper.scss';
+import 'swiper/components/pagination/pagination.scss';
+
+// import Swiper core and required modules
+import SwiperCore, {
+    Pagination
+} from 'swiper';
+
+//Import Images
+import arrow from './../../assets/img/arrow.png';
+import search from './../../assets/img/buscar.png';
+import add from './../../assets/img/mS.png';
+  
+// install Swiper modules
+  SwiperCore.use([Pagination]);
+
+export default function Pets (){
+    return(
+        <div className="Pets-main" >
+            <div>
+                <input className="input-btn" type="text" placeholder="Buscar" onChange={(e) =>{ }}/>
+                <img src={search} alt="seach"/>
+            </div>
+            <div className= "favorite-pets" >
+                <h3>Mis mascotas <img src={add} alt="add"/></h3>
+                <p>Accede al perfil de tus mascotas</p>
+                <div className= "separation">
+                    <Swiper slidesPerView={3} spaceBetween={20} pagination={true} className="mySwiper">
+                        <SwiperSlide>Slide 1</SwiperSlide><SwiperSlide>Slide 2</SwiperSlide><SwiperSlide>Slide 3</SwiperSlide>
+                    </Swiper>
+                </div>
+            </div>
+            <div className="container-btn">
+                <span>Estado de adopcion</span>
+                <img src={arrow} alt="flecha"/>
+            </div>
+            
+
+        </div>
+    )
+}
