7d8ee46c80814b95a17dc87dd2d9535a0b70d89f
Fernando de la Oliva <fernandoliva9@gmail.com>
2021-11-16 15:11:51 +0100

    Add details page, adding data component and health component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Pets from './components/Pets/Pets';
 import Login from "./components/Login/Login";
 import Logout from "./components/Login/Logout";
 import Register from "./components/Login/Register";
+import PetDetails from "./components/Pets/Details/Pets-details";
 
 import "./styles/app.scss";
 import PreLogin from "./components/Login/PreLogin";
@@ -16,13 +17,13 @@ function App() {
       <div className="App">
         <BrowserRouter>
         
-          <Link to="/user/register">|Register|</Link>
+          {/* <Link to="/user/register">|Register|</Link>
           <Link to="/user/login">|Login|</Link>
           <Link to="/user/logout">|Logout|</Link>
           <Link to="/user">|PreLogin|</Link>
           <Link to="/pets">|Pets|</Link>
           <Link to="/home">|Home|</Link>
-          <Link to="/">|Onboarding|</Link>
+          <Link to="/">|Onboarding|</Link> */}
 
           <Routes>
             <Route path="/" element={<Onboarding/>}/>
@@ -32,6 +33,7 @@ function App() {
             <Route path="/user/register" element={<Register />} />
             <Route path="/user/login" element={<Login />} />
             <Route path="/user/logout" element={<Logout />} />
+            <Route path="/pets/details" element={<PetDetails />} />
           </Routes>
         </BrowserRouter>
       </div>
---
+++ src/components/Pets/Details/Pets-details.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useState } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { EffectFade, Pagination } from 'swiper';
+
+import detailpetTest from './../../../assets/img/detailpetTest.png';
+import favIcon from './../../../assets/img/favIcon.png';
+import genreIcon from './../../../assets/img/genreIcon.png';
+import PetData from './Data';
+import PetHealth from './Health';
+
+SwiperCore.use([EffectFade, Pagination]);
+
+const Navbar = () => {
+    
+    const [showData, setShowData] = useState(true);
+
+    if (showData === true){
+        
+    }
+
+    return (
+        <div className="detailPet">
+            <div className="detailPet__sliderImg">
+            <Swiper effect={'fade'} pagination={true} className="mySwiper">
+                <SwiperSlide>
+                    <img src={detailpetTest} alt="detailpetTest" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={detailpetTest} alt="detailpetTest" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={detailpetTest} alt="detailpetTest" />
+                </SwiperSlide>
+            </Swiper>
+            </div>
+            <div className="detailPet__resume">
+                <div className="detail__resume--genre">
+                    <img src={genreIcon} alt="genre" />
+                </div>
+                <div className="detail__resume--data">
+                    <p className="detail__resume--data--name">Blue</p>
+                    <p>Madrid</p>
+                </div>
+                <div className="detail__resume--button">
+                    <img src={favIcon} alt="" />
+                </div>
+            </div>
+            <div className="detailPet__Navbar">
+                <a onClick={() => setShowData(true)}>Datos</a>
+                <a onClick={() => setShowData(false)}>Salud</a>
+            </div>
+            {showData ? (
+                <PetData />
+            ) : (
+                <PetHealth />
+            )}
+
+            <div className="detailPet__Data--button">
+                <a href="#">Adoptar</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
